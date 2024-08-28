260139512c947148079c7e0e2212cf7f272530b0
vaniademianov <vanushademvit@gmail.com>
2024-08-28 18:07:54 +0300

    injecting footer & header

--- base/injector.js
+++ base/injector.js
@@ -1,18 +1,21 @@
-let injectable_header = document.getElementById("injectable-header");
-let injectable_footer = document.getElementById("injectable-footer");
+function inject() {
+  let injectable_header = document.getElementById("injectable-header");
+  let injectable_footer = document.getElementById("injectable-footer");
 
-let header; 
-let footer;
+  let header;
+  let footer;
 
-fetch("base/header.html")
-    .then(response => response.text())
-    .then(data => {
-        header = data;
-        injectable_header.innerHTML = header;
+  fetch("base/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      header = data;
+      injectable_header.innerHTML = header;
     });
-fetch("base/footer.html")
-    .then(response => response.text())
-    .then(data => {
-        footer = data;
-        injectable_footer.innerHTML = footer;
+  fetch("base/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      footer = data;
+      injectable_footer.innerHTML = footer;
     });
+}
+document.addEventListener("DOMContentLoaded", inject)
